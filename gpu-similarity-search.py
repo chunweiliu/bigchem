e756ec469081676fe832c83676c148bc7c54ff20
Chun-Wei Liu <chunwei@stormland.dhcp.unc.edu>
2015-02-09 10:57:15 -0500

    sample code for using cuda

---
+++ gpu-similarity-search.py
@@ -0,0 +1,27 @@
+import pycuda.autoinit
+import pycuda.driver as drv
+import numpy
+import math
+
+from pycuda.compiler import SourceModule
+mod = SourceModule("""
+#include <cmath>
+__global__ void ssd(float *dest, float *a, float *b)
+{
+  const int i = threadIdx.x;
+  dest[i] = sqrt((a[i] - b[i]) * (a[i] - b[i]));  // without square
+}
+""")
+
+ssd = mod.get_function("ssd")
+
+a = numpy.random.randn(400).astype(numpy.float32)
+b = numpy.random.randn(400).astype(numpy.float32)
+
+dest = numpy.zeros_like(a)
+ssd(drv.Out(dest), drv.In(a), drv.In(b),
+    block=(400, 1, 1), grid=(1, 1))
+
+ans = sum(((a - b)*(a - b)) ** 0.5)
+print numpy.sum(dest) - ans
+# print dest-a*b
