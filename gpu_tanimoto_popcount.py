84cca13480b2e74535a889a29cfff19d59b607b1
vancemiller <vbmiller@cs.unc.edu>
2015-02-21 22:49:46 -0500

    Added tanimoto similarity on the GPU.. doesn't work yet. Used ___popcll() to do popcount, not a lookup table. See: http://docs.nvidia.com/cuda/cuda-math-api/group__CUDA__MATH__INTRINSIC__INT.html#group__CUDA__MATH__INTRINSIC__INT_1g6bb2f1cc3495f3f2ebe1df6ec52b3aec

---
+++ gpu_tanimoto_popcount.py
@@ -0,0 +1,47 @@
+import pycuda.autoinit;
+import pycuda.driver as drv;
+import numpy as np;
+import math;
+
+from pycuda.compiler import SourceModule;
+
+mod = SourceModule("""
+        __global__ void tanimoto_popcount(unsigned long long *query, unsigned long long *target, int **out)
+        {
+            int a = 0, b = 0, c = 0;
+            int idy = blockDim.y * blockIdx.y + threadIdx.y;
+            int idx = blockDim.x * blockIdx.x + threadIdx.x;
+            a = __popcll(query[idy]);
+            b = __popcll(target[idx]);
+            c = __popcll(query[idy] & target[idx]);
+            out[idy][idx] = a;//((double) c) / ((double) a + b - c);
+        }
+""");
+
+def GPUtanimoto(query, target, cutoff=0, count=None):
+    query = query.astype(np.uint64);
+    target = target.astype(np.uint64);
+
+    tanimoto = mod.get_function("tanimoto_popcount");
+
+    output = [];
+
+    # check size of GPU memory
+    dest = np.zeros((len(query), len(target)), np.int32);
+    print query;
+    print target;
+    print dest;
+
+    threads_per_block = 1024;
+    blocks_per_mp = 4;
+
+    tanimoto(drv.In(query), drv.In(target), drv.Out(dest), block=(64, 2, 1), grid=(1, 1));
+    print dest;
+    output.append(dest);
+    return output;
+
+if "__main__":
+    target = np.arange(2048, 2056);
+    dest = GPUtanimoto(np.arange(2048, 2050), target);
+    print dest;
+
