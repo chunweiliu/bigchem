5495f562a8f1881bb94cb7d153c265d7ab026bd2
vancemiller <vbmiller@cs.unc.edu>
2015-03-02 10:00:12 -0500

    not working yet but reads from a list of 64 bit integers for each entry

--- gpu_tanimoto_popcount.py
+++ gpu_tanimoto_popcount.py
@@ -6,16 +6,17 @@ import math;
 from pycuda.compiler import SourceModule;
 
 mod = SourceModule("""
-        __global__ void tanimoto_popcount(unsigned long long *query, int query_len, unsigned long long *target, int target_len, double *out)
+        __global__ void tanimoto_popcount(unsigned long long *query, int query_len, unsigned long long *target, int target_len, int length_of_data, double *out)
         {
             int a = 0, b = 0, c = 0;
             int idy = (blockDim.y * blockIdx.y + threadIdx.y);
             int idx = (blockDim.x * blockIdx.x + threadIdx.x);
             if (idy < query_len && idx < target_len) {
-                a = __popcll(query[idy]);
-                b = __popcll(target[idx]);
-                c = __popcll(query[idy] & target[idx]);
-
+                for (int i = 0; i < length_of_data; i++) {
+                    a += __popcll(query[idy]);
+                    b += __popcll(target[idx]);
+                    c += __popcll(query[idy] & target[idx]);
+                }
                 out[idx + idy * blockDim.x] = ((double) c) / ((double) a + b - c);
             }
         }
@@ -34,7 +35,7 @@ def GPUtanimoto(query, target, cutoff=0, count=None):
     print target;
     print dest;
 
-    threads_per_block = 32;
+    threads_per_block = 32; # dependent on hardware
 
     dx, mx = divmod(len(target), threads_per_block);
     dy, my = divmod(len(query), threads_per_block);
@@ -45,12 +46,14 @@ def GPUtanimoto(query, target, cutoff=0, count=None):
     print bdim;
     print gdim;
 
-    tanimoto(drv.In(query), np.int32(len(query)), drv.In(target), np.int32(len(target)), drv.Out(dest), block=bdim, grid=gdim);
+    tanimoto(drv.In(query), np.int32(len(query)), drv.In(target), np.int32(len(target)), np.int32(len(query[0])), drv.Out(dest), block=bdim, grid=gdim);
     return dest;
 
 if "__main__":
-    query = np.arange(0, 2048);
-    target = np.arange(2048, 2056);
+    query = np.array([[x for x in range(y, y + 6)] for y in range(2048, 2050)], np.uint64);
+
+    target = np.array([[x for x in range(y, y + 6)] for y in range(2048, 2050)], np.uint64);
+
     dest = GPUtanimoto(query, target);
     print dest;
 
