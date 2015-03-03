1287650c6d19c301be491d43ebd645eb82f9b0dc
vancemiller <vbmiller@cs.unc.edu>
2015-03-03 13:17:50 -0500

    Working tanimoto popcount similarity for appropriately sized inputs. Need to continue working to ensure large data sets don't crash the program.

--- gpu_tanimoto_popcount.py
+++ gpu_tanimoto_popcount.py
@@ -13,11 +13,14 @@ mod = SourceModule("""
             int idx = (blockDim.x * blockIdx.x + threadIdx.x);
             if (idy < query_len && idx < target_len) {
                 for (int i = 0; i < length_of_data; i++) {
-                    a += __popcll(query[idy]);
-                    b += __popcll(target[idx]);
-                    c += __popcll(query[idy] & target[idx]);
+                    a += __popcll(query[idy * length_of_data + i]);
+                    b += __popcll(target[idx * length_of_data + i]);
+                    c += __popcll(query[idy * length_of_data + i] & target[idx * length_of_data + i]);
                 }
-                out[idx + idy * blockDim.x] = ((double) c) / ((double) a + b - c);
+          /*      if (a + b == c)
+                    out[idx + idy * target_len] = 1.0; // ask about this
+                else*/
+                    out[idx + idy * target_len] = ((double) c) / ((double) a + b - c);
             }
         }
 """);
@@ -31,9 +34,6 @@ def GPUtanimoto(query, target, cutoff=0, count=None):
     output = [];
     # check size of GPU memory
     dest = np.zeros((len(query), len(target)), np.float64);
-    print query;
-    print target;
-    print dest;
 
     threads_per_block = 32; # dependent on hardware
 
@@ -43,17 +43,28 @@ def GPUtanimoto(query, target, cutoff=0, count=None):
     bdim = (threads_per_block, threads_per_block, 1);
     gdim = ((dx + (mx > 0)), (dy + (my > 0)));
 
-    print bdim;
-    print gdim;
+    print("Input query: " + str(query));
+    print("Query length: " + str(len(query)));
+    print("Target data: " + str(target));
+    print("Target length: " + str(len(target)));
+    print("Data length: " + str(len(query[0])));
+    print("Block dimension: " + str(bdim));
+    print("Grid dimension: " + str(gdim));
 
     tanimoto(drv.In(query), np.int32(len(query)), drv.In(target), np.int32(len(target)), np.int32(len(query[0])), drv.Out(dest), block=bdim, grid=gdim);
     return dest;
 
 if "__main__":
-    query = np.array([[x for x in range(y, y + 6)] for y in range(2048, 2050)], np.uint64);
 
-    target = np.array([[x for x in range(y, y + 6)] for y in range(2048, 2050)], np.uint64);
+    query_range = range(2048, 2050);
+    target_range = range(0, 2050);
 
-    dest = GPUtanimoto(query, target);
-    print dest;
+    query = np.array([[x for x in range(y, y + 6)] for y in query_range], np.uint64);
+    target = np.array([[x for x in range(y, y + 6)] for y in target_range], np.uint64);
+
+    # query = np.array([[0xFFF], [0x111], [0x222]]);
+    # target = query;
+
+    output = GPUtanimoto(query, target);
+    print output;
 
